d7ee5667fb87c735d7cc7a3542f4f7d6f4ccbad9
Mihajatiana <miandriakotonerason@gmail.com>
2024-11-30 18:26:51 +0300

    fix size player hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -94,7 +94,7 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 size-36 cursor-pointer overflow-hidden rounded-lg">
             <div
               onClick={handleMiniVdClick}
               className="origin-center scale-50  opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
@@ -105,7 +105,7 @@ const Hero = () => {
                 loop
                 muted
                 id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
+                className="size-36 origin-center scale-150 object-cover object-center"
                 onLoadedData={handleVideoLoad}
               />
             </div>
@@ -116,7 +116,7 @@ const Hero = () => {
             loop
             muted
             id="next-video"
-            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            className="absolute-center invisible absolute z-20 size-36 object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
           <video
